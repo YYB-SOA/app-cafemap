9d150e8d06c33b19930b582907de7a84ec5e2ab0
timbclin <tim.bc.lin@gmail.com>
2022-10-14 21:08:57 +0800

    fix project_info with rubocop

--- lib/project_info.rb
+++ lib/project_info.rb
@@ -1,28 +1,24 @@
 # frozen_string_literal: true
+
 require 'http'
 require 'yaml'
-# frozen_string_literal: true
-
 require 'httparty'
 require 'json'
 require 'net/http'
 require 'uri'
 
-
 config = YAML.safe_load(File.read('config/secrets.yml'))
-
 
 def news_api_path(path)
   "https://newsapi.org/v2/top-headlines?country=tw&apiKey=#{path}"
 end
-key =config['api'][0]['News']
+key = config['api'][0]['News']
 full_url = news_api_path(key)
 # def call_gh_url(config, url)
 #   HTTP.headers('Accept' => 'application/vnd.github.v3+json',
 #                'Authorization' => "token #{config['GITHUB_TOKEN']}").get(url)
 
 # end
-
 
 # def call_news_url(url)
 #     url_path = news_api_path(url)
@@ -35,24 +31,24 @@ full_url = news_api_path(key)
 # end
 
 def call_news_url(url)
-    full = URI(url) 
-    res = Net::HTTP.get_response(full)
-    res.body if res.is_a?(Net::HTTPSuccess)
+  full = URI(url)
+  res = Net::HTTP.get_response(full)
+  res.body if res.is_a?(Net::HTTPSuccess)
 end
 
 news_json = call_news_url(full_url)
 
-print news_json
+# print news_json
 # File.write('lib/sample/news.json', JSON.dump(news_json))
 
 # File.open("lib/sample/news.json", "w") do |f|
 #     f.write(JSON.pretty_generate(news_json))
 #   end
 def hash_to_json(data, dist)
-    # Save it!
-    f_data = File.open(dist,"w")
-    f_data.write(data)
-    f_data.close
+  # Save it!
+  f_data = File.open(dist, 'w')
+  f_data.write(data)
+  f_data.close
 end
 
-hash_to_json(news_json, "lib/sample/news.json")
+hash_to_json(news_json, 'lib/sample/news.json')
