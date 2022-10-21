e5a44a9e5a54e58e053921b7f9d00e46d4ff0304
Leo Shr <gogoloso123@gmail.com>
2022-10-21 15:01:12 +0800

    Note: This file need refactor, and clarify the yaml ds structure issue

--- lib/cafe_api/json_arr2yaml.rb
+++ lib/cafe_api/json_arr2yaml.rb
@@ -7,22 +7,19 @@ require 'json'
 require 'net/http'
 require 'uri'
 
-
 cafenomad_url = 'https://cafenomad.tw/api/v1.2/cafes'
 
 def cafe_url_concat(token)
-    # "https://newsapi.org/v2/top-headlines?country=tw&apiKey=#{token}"
-    token
+  # "https://newsapi.org/v2/top-headlines?country=tw&apiKey=#{token}"
+  token
 end
 
 def get_full_url(token_category, name_of_key)
-    # Added the folder 'config/secrets.yml' first
-    config = YAML.safe_load(File.read('config/secrets.yml'))
-    token = config[token_category][0][name_of_key]
-    cafe_url_concat(token)
+  # Added the folder 'config/secrets.yml' first
+  config = YAML.safe_load(File.read('config/secrets.yml'))
+  token = config[token_category][0][name_of_key]
+  cafe_url_concat(token)
 end
-
-
 
 def call_cafe_url(url)
   uri = URI.parse(url)
@@ -36,13 +33,14 @@ end
 cafe_json = call_cafe_url(cafenomad_url) # return a json array
 
 def json_array_to_yaml(cafe_json)
-    headers = cafe_json[0].keys
-    yaml_key = headers[0] # == 'id'
-    # puts "value_keys: #{value_keys}"
-    # values_num = cafe_json[0].length() -1
-    ## Need Refactor
-    cafe_json.map {|row_data| { row_data[headers[0]] => 
-                                                    {
+  headers = cafe_json[0].keys
+  store_id = headers[0] # == 'id'
+  # puts "value_keys: #{value_keys}"
+  # values_num = cafe_json[0].length() -1
+  ## Need Refactor
+  cafe_json.map do |row_data|
+    { row_data[store_id] =>
+                                                  {
                                                     headers[1] => row_data[headers[1]],
                                                     headers[2] => row_data[headers[2]],
                                                     headers[3] => row_data[headers[3]],
@@ -59,9 +57,9 @@ def json_array_to_yaml(cafe_json)
                                                     headers[14] => row_data[headers[14]],
                                                     headers[15] => row_data[headers[15]],
                                                     headers[16] => row_data[headers[16]],
-                                                    headers[17] => row_data[headers[17]]}
-                                                    }
-                                                }.to_yaml
+                                                    headers[17] => row_data[headers[17]]
+                                                  } }
+  end.to_yaml
 end
 
 # def json_array_to_yaml(cafe_json)
@@ -70,7 +68,7 @@ end
 #     # puts "value_keys: #{value_keys}"
 #     # values_num = cafe_json[0].length() -1
 #     ## Need Refactor
-#     cafe_json.map {|row_data| { row_data[headers[0]] => 
+#     cafe_json.map {|row_data| { row_data[headers[0]] =>
 #                                                     {
 #                                                     headers[1] => row_data[headers[1]],
 #                                                     headers[2] => row_data[headers[2]],
@@ -92,21 +90,18 @@ end
 #                                                     }
 #                                                 }.to_yaml
 # end
- 
- 
- 
-def save_yaml(yaml_hash, path)
-    File.open(path, "w") { |file| file.write(yaml_hash) }
-end
 
+def save_yaml(yaml_hash, path)
+  File.write(path, yaml_hash)
+end
 
 def main(token_category, name_of_key, output_path)
-    cafenomad_url = get_full_url(token_category,name_of_key)
-    cafe_json = call_cafe_url(cafenomad_url)
-    cafe_yaml = json_array_to_yaml(cafe_json)
-    save_yaml(cafe_yaml, output_path)
-    cafe_yaml
+  cafenomad_url = get_full_url(token_category, name_of_key)
+  cafe_json = call_cafe_url(cafenomad_url)
+  cafe_yaml = json_array_to_yaml(cafe_json)
+  save_yaml(cafe_yaml, output_path)
+  cafe_yaml
 end
 
-cafe_yaml = main("CAFE_NOMAD","Cafe_api",  "db/sample/cafe_nomad.yml")
+main('CAFE_NOMAD', 'Cafe_api', 'db/sample/cafe_nomad.yml')
 # question: 我的yaml 用ID做head分隔資料，這樣若對place_api folder中shop 或是comment設計時是否會造成困難?
