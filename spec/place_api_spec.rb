ad52aea381f45c8cc6e7f32ed19fef99d63f230f
Leo Shr <gogoloso123@gmail.com>
2022-10-22 20:15:46 +0800

    RUBOCOP: place_api_spec.rb

--- spec/place_api_spec.rb
+++ spec/place_api_spec.rb
@@ -6,7 +6,6 @@ describe 'Tests Place API library' do
   VCR.configure do |c|
     c.cassette_library_dir = CASSETTES_FOLDER
     c.hook_into :webmock
-
     c.filter_sensitive_data('<Place_TOKEN>') { PLACE_TOKEN }
     c.filter_sensitive_data('<Place_TOKEN_ESC>') { CGI.escape(PLACE_TOKEN) }
   end
@@ -23,12 +22,10 @@ describe 'Tests Place API library' do
 
   describe 'Store information' do
     it 'HAPPY: should provide correct Shop attributes' do
-        # PlaceApi會繼承module Store class 的特定參數，目前store檔案只有傳入store_yaml，可能不適合
-        # 老師範例中有額外傳入data_source，不知道是啥意思
-        shop = PlaceInfo::PlaceApi.new(PLACE_TOKEN)
-                                     .store(??, filter_)
-     # 以下兩行根據shop_info傳入place_results['變數名稱'] (yml檔案)替換size與git_url
-
+      # PlaceApi會繼承module Store class 的特定參數，目前store檔案只有傳入store_yaml，可能不適合
+      # 老師範例中有額外傳入data_source，不知道是啥意思
+      shop = PlaceInfo::PlaceApi.new(PLACE_TOKEN).store('?', filter_)
+      # 以下兩行根據shop_info傳入place_results['變數名稱'] (yml檔案)替換size與git_url
       _(shop.headers).must_equal CORRECT['result']['header']
       _(shop.location).must_equal CORRECT['result']['geometry']['location']
     end
@@ -59,7 +56,7 @@ describe 'Tests Place API library' do
     #   _(@store.owner.storename).wont_be_nil
     #   _(@store.owner.storename).must_equal CORRECT['results']['name']
     # end
-#--------------------------review/feedback-----------
+    #--------------------------review/feedback-----------
     it 'HAPPY: should identify reviews number' do
       reviews = @store.reviews
       _(reviews.count).must_equal CORRECT['reviews'].count
