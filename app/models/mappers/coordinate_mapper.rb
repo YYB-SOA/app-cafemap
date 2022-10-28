1c27bc2f28bebdf64edb876062952e9d24cf263e
Leo Shr <gogoloso123@gmail.com>
2022-10-28 16:19:45 +0800

    DELETE: DB & .gitignore log file modified:   .gitignore modified:   app/models/mappers/coordinate_mapper.rb deleted:    db/sample/cafe_nomad.json deleted:    db/sample/cafe_nomad.yml deleted:    db/sample/cafe_nomad1.json deleted:    db/sample/cafe_nomad1.yml deleted:    db/sample/cafe_nomad2.json deleted:    db/sample/cafe_nomad3.yml deleted:    db/sample/cafe_nomad9.json deleted:    db/sample/news.json deleted:    db/sample/news_backup.json

--- app/models/mappers/coordinate_mapper.rb
+++ app/models/mappers/coordinate_mapper.rb
@@ -1,43 +1,54 @@
 # frozen_string_literal: false
-
-require_relative '' # 要require 元詰的mapper
-require_relative '' # 要require Tim的mapper
+require_relative "../gateways/cafe_api.rb"
+require_relative 'info_mapper' 
 
 module Transfer
   # Provides access to contributor data
-  module Store
-    # 資料Entity類別名為module
+  module StoreMapper
     class Data_Input
-    # 拉資料
-      def initialize(word_term)
-        @word_term = word_term
+    # 拉user input word term 資料
+      def initialize(wordterm)
+        @wordterm = wordterm
       end
 
-      def get_wordterm(@word_term)
-        @word_term
+      def get_wordterm()
+        @wordterm
       end
 
-      def get_nomad
+      def get_nomad()
         # return string array
-        CafeNomad::ApiInfo.new(@word_term).get_store_name
+        CafeMap::CafeNomad::InfoMapper.new("Cafe_api").load_several
       end
     end
-
     class Data_Output
+      @@fake_store = ['R5小餐館','蜂巢咖啡','KURUMI哭哭咖啡二店','橋恩咖啡','心尚咖啡館','Fuelwood Coffee 燃木咖啡研究所','義大利咖啡師訓練中心']
+
         nomad_store = Transfer::Store.Data_Input.get_nomad
         user_wordterm = Transfer::Store.Data_Input.get_wordterm
-        def initialize(nomad_store, user_wordterm)
+        def initialize(user_wordterm, nomad_store = @@fake_store)
             @nomad_store = nomad_store 
-            @user_wordterm = user_wordterm 
-        end    
+            @user_wordterm = user_wordterm
+        end
 
         def filterArray
             @nomad_store
         end
 
-        def filterData(word_term =  @user_wordterm, filterArray = @nomad_store)
-            filterArray.includes?? (word_term) == true, word_term : "Warming: It's not ligit word term."        
+        def filtered_store(word_term =  @user_wordterm, filterArray = @nomad_store)
+            filterArray.includes(word_term)? word_term : "Warming: It's not ligit word term."  
         end
+
     end
   end
 end
+
+# CafeNomad::CafeApi::InfoMapper.new(token, gateway_class = CafeNomad::Api).build_entity()
+# find(token, gateway_class = CafeNomad::Api)
+word_term = "Cafe_api"
+temp1 = Transfer::StoreMapper::Data_Output.new(word_term)
+puts temp1.filterData
+# data = CafeMap::CafeNomad::Api
+# a = CafeMap::CafeNomad::InfoMapper.new(word_term).load_several
+# puts a.load_several
+
+
