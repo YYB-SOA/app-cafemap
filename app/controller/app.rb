ab491e59bba640ca124178f75c87617157d4f4ab
Fong <fong54388@gmail.com>
2022-10-30 03:06:24 +0800

    UPDATE stores_data disploy

--- app/controller/app.rb
+++ app/controller/app.rb
@@ -26,34 +26,39 @@ module CafeMap
         view 'home' #, locals: { topics: stores_data }
       end
 
-      routing.on 'storelist' do
+      routing.on 'region' do
         routing.is do
           # POST /storelist/
-          routing.post do
+          
+          # routing.params['欲查詢的地區']
+          stores_data[4].address
+
+          # routing.post do
             # Show error 400 if the  user input is illigal
-            user_wordterm =routing.params['region']
-            city_arr = %w[新竹 台北 宜蘭 臺北 新北 桃園 苗栗 台中 嘉義 台南 台東 花蓮 南投]
-            routing.halt 404 unless city_arr.any?(wordterm) &&
-                                   (wordterm.split(/ /).count >= 2)
+            # user_wordterm = routing.params['region']
+            # city_arr = %w[新竹 台北 宜蘭 臺北 新北 桃園 苗栗 台中 嘉義 台南 台東 花蓮 南投]
+            # routing.halt 404 unless city_arr.any?(user_wordterm) &&
+            #                        (user_wordterm.split(/ /).count >= 2)
             
-            # Filtered
-            region = city_arr.select{ |city| city== user_wordterm}
+            # stores_data            
+            # # Filtered
+            # region = city_arr.select{ |city| city== user_wordterm}
 
             # stores_list = stores_data.select { |obj| obj.address.include? user_wordterm }.map(&:name)
             
-            routing.redirect "Cafe-Map/regional"
-            end
+            # routing.redirect "Cafe-Map/region"
+            # end
+          
           end
-        routing.is do
-          # GET /cafe/storename
-          routing.get do
-            cafe_storename = CafeMap::InfoMapper
-                             .new(CAFE_TOKEN_NAME)
-                             .load_several
-            view 'storelist', locals: { storelist: region }
-
-          end
-        end
+        # routing.is do
+        #   # GET /cafe/region
+        #   routing.get do
+        #     cafe_storename = CafeMap::InfoMapper
+        #                      .new(CAFE_TOKEN_NAME)
+        #                      .load_several
+        #     view 'region', locals: { storelist: region }
+        #   end
+        # end
       end
     end
   end
