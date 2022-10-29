f85b2fd08acedce23446907c310cdb425cf8d67a
Leo Shr <gogoloso123@gmail.com>
2022-10-29 16:43:05 +0800

    MODIFIED:    modified:   app/controller/app.rb  new file:   app/controller/test.rb  new file:   app/views/404.slim,  modified:   app/views/assets/css/style.css, deleted:    app/views/project.slim ,  new file:   app/views/storelist.slim

--- app/controller/app.rb
+++ app/controller/app.rb
@@ -10,38 +10,45 @@ module CafeMap
     plugin :assets, css: 'style.css', path: 'app/views/assets'
     plugin :common_logger, $stderr
     plugin :halt
+    plugin :status_handler
 
+    status_handler(404) do
+      view('404')
+    end
     route do |routing|
       routing.assets # load CSS
       response['Content-Type'] = 'text/html; charset=utf-8'
 
-      stores_data = CafeMap::CafeNomad::InfoMapper
-
+      stores_data = CafeMap::CafeNomad::InfoMapper.new(CAFE_TOKEN_NAME).load_several
+      
       # GET /
       routing.root do
-        view 'home'
+        view 'home' #, locals: { topics: stores_data }
       end
 
-      routing.on 'project' do
+      routing.on 'storelist' do
         routing.is do
-          # POST /project/
+          # POST /storelist/
           routing.post do
-           
-            region=routing.params['region']
-            # city_arr = %w[新竹 台北 宜蘭 臺北 新北 桃園 苗栗 台中]
-            # region.halt 400 unless city_arr.any?(region) &&
-            #                        (region.split(/ /).count >= 2)
-            routing.redirect "yyb/#{region}/"
+            # Show error 400 if the  user input is illigal
+            user_wordterm =routing.params['region']
+            city_arr = %w[新竹 台北 宜蘭 臺北 新北 桃園 苗栗 台中 嘉義 台南 台東 花蓮 南投]
+            routing.halt 404 unless city_arr.any?(wordterm) &&
+                                   (wordterm.split(/ /).count >= 2)
+            
+            # Filtered
+            region = city_arr.select{ |city| city== user_wordterm}
+
+            # stores_list = stores_data.select { |obj| obj.address.include? user_wordterm }.map(&:name)
+            
+            routing.redirect "Cafe-Map/regional"
             end
           end
-
         routing.is do
           # GET /cafe/storename
           routing.get do
-            # cafe_storename = CafeMap::InfoMapper
-            #                  .new(CAFE_TOKEN_NAME)
-            #                  .load_several
-            # view 'project', locals: { project: region }
+
+            view 'storelist' #, locals: { storelist: region }
 
           end
         end
