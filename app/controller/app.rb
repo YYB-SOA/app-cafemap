adb12001c47e6980e0b5c7728c9e33a463ebce9d
Fong <fong54388@gmail.com>
2022-10-30 03:52:54 +0800

    FIXED url display

--- app/controller/app.rb
+++ app/controller/app.rb
@@ -23,31 +23,29 @@ module CafeMap
       
       # GET /
       routing.root do
-        view 'home' #, locals: { topics: stores_data }
+        view 'home' # , locals: { store_name: stores_data }
       end
 
       routing.on 'region' do
         routing.is do
           # POST /storelist/
-          
-          # routing.params['欲查詢的地區']
-          stores_data[4].address
 
-          # routing.post do
-            # Show error 400 if the  user input is illigal
-            # user_wordterm = routing.params['region']
+          routing.post do
+            user_wordterm = routing.params['欲查詢的地區']
+            city = stores_data.find { |store| store.address.include? user_wordterm }
+            routing.halt 404 unless city
+            routing.redirect "light-of-day/#{city.city}"
             # city_arr = %w[新竹 台北 宜蘭 臺北 新北 桃園 苗栗 台中 嘉義 台南 台東 花蓮 南投]
             # routing.halt 404 unless city_arr.any?(user_wordterm) &&
             #                        (user_wordterm.split(/ /).count >= 2)
-            
-            # stores_data            
-            # # Filtered
+                   
+            # Filtered
             # region = city_arr.select{ |city| city== user_wordterm}
 
             # stores_list = stores_data.select { |obj| obj.address.include? user_wordterm }.map(&:name)
             
             # routing.redirect "Cafe-Map/region"
-            # end
+            end
           
           end
         # routing.is do
