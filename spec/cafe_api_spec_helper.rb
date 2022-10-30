2a8512fb87417227626d1180b82cc50c6e975dcf
Leo Shr <gogoloso123@gmail.com>
2022-10-30 16:49:25 +0800

    Fix: spec/cafe_api_spec_helper.rb

--- spec/cafe_api_spec_helper.rb
+++ spec/cafe_api_spec_helper.rb
@@ -10,12 +10,22 @@ require 'minitest/rg'
 require 'vcr'
 require 'webmock'
 
-require_relative '../lib/cafe_api/cafe_api'
+require_relative '../lib/mappers/info_mapper'
 CONFIG = YAML.safe_load(File.read('config/secrets.yml'))
 
-CAFE_TOKEN = 'Cafe_api' # CONFIG['CAFE_NOMAD'][0]['Cafe_api']
-CORRECT = YAML.safe_load(File.read('db/sample/cafe_nomad3.yml'))
+CAFE_TOKEN = CONFIG['CAFE_NOMAD'][0]['Cafe_api']
+
+CORRECT = YAML.safe_load(File.read('spec/fixtures/cafe_results.yml'))
 FAKE_TOKEN = 'Fake_api'
 
 CASSETTES_FOLDER = 'spec/fixtures/cassettes'
 CASSETTE_FILE = 'cafe_api'
+
+def ans_sheet(target_attr, data_keys)
+  # This func is to replace the original duplicate code below:
+  # @yaml_keys.map { |item| CORRECT[item]['id'] }
+
+  data_keys.map do |item|
+    CORRECT[item][target_attr]
+  end
+end
