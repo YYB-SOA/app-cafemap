1907bb652b0226085624f4e510acbd11446759a4
Leo Shr <gogoloso123@gmail.com>
2022-10-30 16:51:00 +0800

    UPDATE: cafe_api_spec as data_mapper branch

--- spec/cafe_api_spec.rb
+++ spec/cafe_api_spec.rb
@@ -21,97 +21,86 @@ describe 'Tests Cafe Nomad API library' do
     VCR.eject_cassette
   end
 
-  describe 'Store information' do
-    it 'HAPPY: should provide correct api status attributes' do
-      # PlaceApi會繼承module Store class 的特定參數，目前store檔案只有傳入store_yaml，可能不適合
-      # 老師範例中有額外傳入data_source，不知道是啥意思
-      api_status = CafeNomad::CafeApi.new('CAFE_NOMAD', CAFE_TOKEN).api_status
-      _(api_status.status).must_equal CORRECT['status']
-      _(api_status.amount).must_equal CORRECT['amount']
-      _(api_status.header).must_equal CORRECT['header']
-    end
-    # it 'SAD: should provide correct TOKEN_NAME' do
-    #   CafeNomad::CafeApi.new("CAFE_NOMAD", FAFE_TOKEN).api_status.response_nil?.must_equal true
-    # end
-    # it 'SAD: should raise exception on incorrect project' do
-    #   _(proc do
-    #     CafeNomad::CafeApi.new("CAFE_NOMAD", FAFE_TOKEN)
-    #   end).must_raise CodePraise::GithubApi::Response::NotFound
-    # end
-  end
-  describe 'cafe nomad  information' do
+  describe 'Cafe Nomad Store Content' do
     before do
-      @api_info = CafeNomad::CafeApi.new('CAFE_NOMAD', CAFE_TOKEN).api_info
-      @cafe_yaml_keys = CORRECT.keys[3..]
+      @cafe_info = CafeMap::CafeNomad:: InfoMapper.new('Cafe_api').load_several
+      @yaml_keys = CORRECT.keys[3..]
     end
-    ##   Owner Role we don't have this kind of role
-    # it 'HAPPY: should recognize owner' do
-    #   _(@store.owner).must_be_kind_of PlaceInfo::Reviews
-    # end
 
-    # it 'HAPPY: should identify owner' do
-    #   _(@store.owner.storename).wont_be_nil
-    #   _(@store.owner.storename).must_equal CORRECT['results']['name']
-    # end
-    #--------------------------review/feedback-----------
+    #--------------------------Store Content-----------
+    it 'HAPPY: should pass corect id' do
+      _(@cafe_info.map(&:id)).must_equal ans_sheet('id', @yaml_keys)
+    end
 
-    it 'HAPPY: should recognize owner' do
-      _(@api_info.map(&:id)).must_equal @cafe_yaml_keys.map { |item| CORRECT[item]['id'] }
+    it 'HAPPY: should pass corect name' do
+      _(@cafe_info.map(&:name)).must_equal ans_sheet('name', @yaml_keys)
+
     end
-    it 'HAPPY: should recognize owner' do
-      _(@api_info.map(&:name)).must_equal @cafe_yaml_keys.map { |item| CORRECT[item]['name'] }
+
+    it 'HAPPY: should pass corect city' do
+      _(@cafe_info.map(&:city)).must_equal ans_sheet('city', @yaml_keys)
     end
-    it 'HAPPY: should recognize owner' do
-      _(@api_info.map(&:city)).must_equal @cafe_yaml_keys.map { |item| CORRECT[item]['city'] }
+
+    it 'HAPPY: should pass corect url' do
+      _(@cafe_info.map(&:url)).must_equal ans_sheet('url', @yaml_keys)
     end
-    it 'HAPPY: should recognize owner' do
-      _(@api_info.map(&:wifi)).must_equal @cafe_yaml_keys.map { |item| CORRECT[item]['wifi'] }
+
+    it 'HAPPY: should pass corect address' do
+      _(@cafe_info.map(&:address)).must_equal ans_sheet('address', @yaml_keys)
     end
-    it 'HAPPY: should recognize owner' do
-      _(@api_info.map(&:seat)).must_equal @cafe_yaml_keys.map { |item| CORRECT[item]['seat'] }
+
+    it 'HAPPY: should pass corect latitude' do
+      _(@cafe_info.map(&:latitude)).must_equal ans_sheet('latitude', @yaml_keys)
     end
-    it 'HAPPY: should recognize owner' do
-      _(@api_info.map(&:quiet)).must_equal @cafe_yaml_keys.map { |item| CORRECT[item]['quiet'] }
+
+    it 'HAPPY: should pass corect longitude' do
+      _(@cafe_info.map(&:longitude)).must_equal ans_sheet('longitude', @yaml_keys)
     end
-    it 'HAPPY: should recognize owner' do
-      _(@api_info.map(&:tasty)).must_equal @cafe_yaml_keys.map { |item| CORRECT[item]['tasty'] }
+
+    it 'HAPPY: should pass corect limited_time' do
+      _(@cafe_info.map(&:limited_time)).must_equal ans_sheet('limited_time', @yaml_keys)
     end
-    it 'HAPPY: should recognize owner' do
-      _(@api_info.map(&:cheap)).must_equal @cafe_yaml_keys.map { |item| CORRECT[item]['cheap'] }
+
+    it 'HAPPY: should pass corrected  mrt' do
+      _(@cafe_info.map(&:mrt)).must_equal ans_sheet('mrt', @yaml_keys)
     end
-    it 'HAPPY: should recognize owner' do
-      _(@api_info.map(&:music)).must_equal @cafe_yaml_keys.map { |item| CORRECT[item]['music'] }
+
+    it 'HAPPY: should pass corrected  open_time' do
+      _(@cafe_info.map(&:open_time)).must_equal ans_sheet('open_time', @yaml_keys)
     end
-    it 'HAPPY: should recognize owner' do
-      _(@api_info.map(&:url)).must_equal @cafe_yaml_keys.map { |item| CORRECT[item]['url'] }
+
+    it 'HAPPY: should pass identitied standing_desk' do
+      _(@cafe_info.map(&:standing_desk)).must_equal ans_sheet('standing_desk', @yaml_keys)
     end
-    it 'HAPPY: should recognize owner' do
-      _(@api_info.map(&:address)).must_equal @cafe_yaml_keys.map { |item| CORRECT[item]['address'] }
+
+    it 'HAPPY: should pass identitied socket' do
+      _(@cafe_info.map(&:socket)).must_equal ans_sheet('socket', @yaml_keys)
     end
-    it 'HAPPY: should recognize owner' do
-      _(@api_info.map(&:latitude)).must_equal @cafe_yaml_keys.map { |item| CORRECT[item]['latitude'] }
+
+    #--------------------------Rating Content---------------------------
+
+    it 'HAPPY: should pass identitied wifi score' do
+      _(@cafe_info.map(&:wifi)).must_equal ans_sheet('wifi', @yaml_keys).map(& :to_s)
     end
-    it 'HAPPY: should recognize owner' do
-      _(@api_info.map(&:longitude)).must_equal @cafe_yaml_keys.map { |item| CORRECT[item]['longitude'] }
+
+    it 'HAPPY: should pass identitied seat score' do
+      _(@cafe_info.map(&:seat)).must_equal ans_sheet('seat', @yaml_keys).map(& :to_s)
     end
-    it 'HAPPY: should recognize owner' do
-      _(@api_info.map(&:limited_time)).must_equal @cafe_yaml_keys.map { |item|
-                                                    CORRECT[item]['limited_time']
-                                                  }
+
+    it 'HAPPY: should pass identitied quiet score' do
+      _(@cafe_info.map(&:quiet)).must_equal ans_sheet('quiet', @yaml_keys).map(& :to_s)
     end
-    it 'HAPPY: should recognize owner' do
-      _(@api_info.map(&:socket)).must_equal @cafe_yaml_keys.map { |item| CORRECT[item]['socket'] }
+
+    it 'HAPPY: should pass identitied tasty score' do
+      _(@cafe_info.map(&:tasty)).must_equal ans_sheet('tasty', @yaml_keys).map(& :to_s)
     end
-    it 'HAPPY: should recognize owner' do
-      _(@api_info.map(&:standing_desk)).must_equal @cafe_yaml_keys.map { |item|
-                                                     CORRECT[item]['standing_desk']
-                                                   }
+
+    it 'HAPPY: should pass identitied cheap score' do
+      _(@cafe_info.map(&:cheap)).must_equal ans_sheet('cheap', @yaml_keys).map(& :to_s)
     end
-    it 'HAPPY: should recognize owner' do
-      _(@api_info.map(&:mrt)).must_equal @cafe_yaml_keys.map { |item| CORRECT[item]['mrt'] }
-    end
-    it 'HAPPY: should recognize owner' do
-      _(@api_info.map(&:open_time)).must_equal @cafe_yaml_keys.map { |item| CORRECT[item]['open_time'] }
+
+    it 'HAPPY: should pass identitied music score' do
+      _(@cafe_info.map(&:music)).must_equal ans_sheet('music', @yaml_keys).map(& :to_s)
     end
   end
 end
