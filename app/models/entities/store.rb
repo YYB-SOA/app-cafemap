c029018460b5a863d6029e90d97c5a6a013a05cf
Leo Shr <gogoloso123@gmail.com>
2022-11-06 10:35:46 +0800

    Issue w6

---
+++ app/models/entities/store.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: false
+
+require 'dry-types'
+require 'dry-struct'
+
+module CafeMap
+  module Entity
+    # Domain entity for stores
+    class Store < Dry::Struct
+      include Dry.Types
+
+      attribute :place_id, Strict::String  # Coercible #Strict
+      attribute :name, Strict::String
+      attribute :formatted_address, Strict::String
+      attribute :location_lat,  Strict::Float
+      attribute :location_lng,  Strict::Float
+      attribute :rating, Nominal::Float
+      attribute :user_ratings_total, Strict::Integer
+    end
+  end
+end
