5f35311e5ea9cc621b9245ce88efdfbe63883d71
Leo Shr <gogoloso123@gmail.com>
2022-11-06 15:14:15 +0800

    FIX:APP in shiuan-dev-db5 branch

--- app/models/repositories/infos.rb
+++ app/models/repositories/infos.rb
@@ -34,10 +34,10 @@ module CafeMap
       end
 
       def self.create(entity)
-        raise 'Project already exists' if find(entity)
-
-        db_info = PersistInfo.new(entity).create_info
-        rebuild_entity(db_info)
+        unless find(entity)
+          db_info = PersistInfo.new(entity).create_info
+          rebuild_entity(db_info)
+        end
       end
 
       def self.rebuild_entity(db_record)
