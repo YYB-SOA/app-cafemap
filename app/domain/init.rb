12826caba82f29aac021e0b2c2f49da7b3a99795
Leo Shr <gogoloso123@gmail.com>
2022-11-13 21:44:25 +0800

    FIX: routor and front end parsing

--- app/domain/init.rb
+++ app/domain/init.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-folders = %w[entities repositories mappers]
+folders = %w[cafenomad googleplace recommendations]
 folders.each do |folder|
   Dir.glob("#{__dir__}/#{folder}/**/*.rb").each do |file|
     require_relative file
