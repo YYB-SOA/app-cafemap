46b22ca01e75936abbff75cc5f1262ec109d17da
timbclin <tim.bc.lin@gmail.com>
2022-11-14 10:06:13 +0800

    Update

--- spec/helpers/spec_helper.rb
+++ spec/helpers/spec_helper.rb
@@ -5,37 +5,37 @@ ENV['RACK_ENV'] = 'test'
 # require 'simplecov'
 # SimpleCov.start
 
-require 'yaml'
-require 'minitest/autorun'
-require 'minitest/rg'
-require 'vcr'
-require 'webmock'
-require_relative '../../require_app'
-require_relative '../../app/domain/cafenomad/mappers/info_mapper.rb'
-require_relative '../../config/environment'
+# require 'yaml'
+# require 'minitest/autorun'
+# require 'minitest/rg'
+# require 'vcr'
+# require 'webmock'
+# require_relative '../../require_app'
+# require_relative '../../app/domain/cafenomad/mappers/info_mapper.rb'
+# require_relative '../../config/environment'
 # require_app
 
-CONFIG = YAML.safe_load(File.read('config/secrets.yml'))
+# CONFIG = YAML.safe_load(File.read('config/secrets.yml'))
 
-CAFE_TOKEN = CafeMap::App.config.CAFE_TOKEN
-# puts CAFE_TOKEN
+# CAFE_TOKEN = CafeMap::App.config.CAFE_TOKEN
+# # puts CAFE_TOKEN
 
-CAFE_CORRECT = YAML.safe_load(File.read('spec/fixtures/cafe_results.yml'))
-FAKE_TOKEN = 'Fake_api'
+# CAFE_CORRECT = YAML.safe_load(File.read('spec/fixtures/cafe_results.yml'))
+# FAKE_TOKEN = 'Fake_api'
 
-# PLACE_API
+# # PLACE_API
 
-KEYWORD_FILTER = '新竹'
-TOKEN_NAME = 'Place_api'
+# KEYWORD_FILTER = '新竹'
+# TOKEN_NAME = 'Place_api'
 
 
-PLACE_TOKEN = CafeMap::App.config.PLACE_TOKEN
-# puts PLACE_TOKEN
-TEST_STORE = ["WHO'S 喜象 CAFE", 'ARTROOM14藝室']
-FAKE_TEST_STORE = ['']
-PLACE_CORRECT = YAML.safe_load(File.read('spec/fixtures/place_results.yml'))
-STORE_CORRECT = PLACE_CORRECT.keys[0..]
-INCORRECT = YAML.safe_load(File.read('spec/fixtures/place_bad_results.yml'))
+# PLACE_TOKEN = CafeMap::App.config.PLACE_TOKEN
+# # puts PLACE_TOKEN
+# TEST_STORE = ["WHO'S 喜象 CAFE", 'ARTROOM14藝室']
+# FAKE_TEST_STORE = ['']
+# PLACE_CORRECT = YAML.safe_load(File.read('spec/fixtures/place_results.yml'))
+# STORE_CORRECT = PLACE_CORRECT.keys[0..]
+# INCORRECT = YAML.safe_load(File.read('spec/fixtures/place_bad_results.yml'))
 
 
 
