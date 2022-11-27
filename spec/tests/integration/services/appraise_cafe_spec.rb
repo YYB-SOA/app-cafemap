5e2d9826bede8546ce25506075402692e434c690
Leo Shr <gogoloso123@gmail.com>
2022-11-28 02:19:05 +0800

    DELETE: deleted unfinished spec file for Tim

--- spec/tests/integration/services/appraise_cafe_spec.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-require_relative '../../../helpers/spec_helper.rb'
-require_relative '../../../helpers/vcr_helper.rb'
-require_relative '../../../helpers/database_helper.rb'
-
